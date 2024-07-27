cb2785666fcafe78396227a814bf47297b52518e
surya <varma.surya@protonmail.com>
2024-07-27 20:59:48 +0530

    added mongodb connection way-one

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+// import express from "express";
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERRR: ", error);
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("ERROR:", error);
+//   }
+// })();
