d4e564f3bd262fbc7d5d6b57fea674aa74092a62
surya <varma.surya@protonmail.com>
2024-07-28 23:58:48 +0530

    added login and logout features and tokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logOutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -17,5 +22,10 @@ userRouter.route("/register").post(
   ]),
   registerUser
 );
+
+userRouter.route("/login").post(loginUser);
+
+//securedRoutes
+userRouter.route("/logout").post(verifyJWT, logOutUser);
 
 export default userRouter;
