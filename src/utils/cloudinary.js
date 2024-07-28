4df6fc9b73d43394a41e005e46f078625e70acf3
surya <varma.surya@protonmail.com>
2024-07-28 18:44:39 +0530

    updated bug regarding CoverImage

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file has been uploaded
-    console.log("File has been uploaded on Cloudinary", response.url);
+    // console.log("File has been uploaded on Cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temp file as the upload operation got failed
