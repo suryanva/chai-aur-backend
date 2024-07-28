f99418daa78d3b2246dcfb18d30595f600d9704d
surya <varma.surya@protonmail.com>
2024-07-28 17:03:28 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -19,4 +19,14 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//router declaration
+
+app.use("/api/v1/users", userRouter);
+
+//http://locolhost:8000/api/v1/users/register
+
 export { app };
