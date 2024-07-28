339b652c3ebeb43278fe7d52f2f08fb0eeb42d1e
surya <varma.surya@protonmail.com>
2024-07-28 09:29:11 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 // import express from "express";
 
@@ -24,4 +25,12 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port :${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED!!!", err);
+  });
